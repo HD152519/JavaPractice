a0749ffd90104655eff74177a5ea05101f00c881
신주영 <@DESKTOP-GGS5H8B>
2017-06-02 23:25:47 +0900

    제출합니다

--- JavaPractice/src/org/dimigo/collection/MelonChart.java
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -3,6 +3,8 @@
  */
 package org.dimigo.collection;
 
+import java.util.List;
+
 /**
  * <pre>
  * org.dimigo.collection
@@ -17,12 +19,39 @@ package org.dimigo.collection;
  */
 public class MelonChart {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+        List<Music> list = new ArrayList<>();
 
-	}
+        list.add(new Music("I LUV IT", "PSY"));
+        list.add(new Music("맞지?", "언니쓰"));
+
+        System.out.println("-- << 멜론 챠트 >> --");
+        printLIst(list);
+
+        System.out.println("-- << 2위 곡 추가 >> --");
+        list.add(1, new Music("SIGNAL", "트와이스"));
+        printLIst(list);
+
+        System.out.println("-- << 3위 곡 변경 >> --");
+        list.set(2, new Music("팔레트", "아이유"));
+        printLIst(list);
+
+        System.out.println("-- << 2위 곡 삭제 >> --");
+        list.remove(1);
+        printLIst(list);
+
+        System.out.println("-- << 전체 리스트 삭제 >> --");
+        list.clear();
+        printLIst(list);
+    }
+
+    public static void printLIst(List<Music> list) {
+        int i = 1;
+        for (Music a: list) {
+            System.out.println(i++ + ". " + a.toString());
+        }
+        System.out.println();
+    }
 
 }
