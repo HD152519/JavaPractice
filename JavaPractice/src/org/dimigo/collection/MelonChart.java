e6a8edd9471f3b575d764efa19c3437989414a3b
신주영 <@DESKTOP-GGS5H8B>
2017-06-02 23:21:29 +0900

    제출합니다.

---
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+/**
+ * <pre>
+ * org.dimigo.collection
+ *		|_ MelonChart
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 2.
+ * </pre>
+ *
+ * @author		: 신주영
+ * @version 	: 1.0
+ */
+public class MelonChart {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/collection/Music.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+/**
+ * <pre>
+ * org.dimigo.collection
+ *		|_ Music
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 2.
+ * </pre>
+ *
+ * @author		: 신주영
+ * @version 	: 1.0
+ */
+public class Music {
+
+}
